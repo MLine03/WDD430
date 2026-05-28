d3e3e4b2b09fa6eb7e86cd0d8598f3a3f5347680
Your Name <yourgithubemail@email.com>
2026-05-28 16:10:14 -0600

    Setup Next.js project and added Navbar

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
-<<<<<<< HEAD
 import type { NextConfig } from "next";
-=======
-import type { NextConfig } from 'next';
->>>>>>> c13c237c4e9c2ebf4e88e01c1a0040171db44ba0
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
 };
 
 export default nextConfig;
